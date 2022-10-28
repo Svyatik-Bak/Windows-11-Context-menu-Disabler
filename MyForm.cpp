986320c1408783e7c2cdf14a22849ca8c1d12df7
Svyatoslav Bak <89962566+Svyatik-Bak@users.noreply.github.com>
2022-10-28 10:35:22 +0000

    Delete MyForm.cpp

--- MyForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm.h"
-#include <Windows.h>
-using namespace Windows11ContextMenuDisablerCpp;
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MyForm);
-	return 0;
-}
